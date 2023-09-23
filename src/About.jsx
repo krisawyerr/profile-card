8c15d35d8e23601056c7a3cc4db16aca254b9216
krisawyerr <krisawyerrr@gmail.com>
2023-09-23 10:06:32 -0700

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="AboutSection">
+            <h1 className="WordAbout">About</h1>
+            <p className="AboutParagraph">I am a cybersecurity major who has embarked on a journey into the world of front-end web development. Exploring the creative aspects of web development, I've been drawn to the art of crafting user-friendly and visually engaging web interfaces. </p>
+        </div>
+    )
+}
