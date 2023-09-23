8c98ad96d7bedef18175f5c6948660d10f4ce41b
Kris Sawyerr <krisawyerr@gmail.com>
2023-09-23 10:35:54 -0700

    fixed images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <div className="Footer">
-            <a href="https://github.com/"><img src="/src/icons8-github-80.png" alt="" width={40}/></a>
-            <a href="https://www.instagram.com/"><img src="/src/icons8-instagram-80 (1).png" alt="" width={40}/></a>
-            <a href="https://www.facebook.com/"><img src="/src/icons8-facebook-80.png" alt="" width={40}/></a>
-            <a href="https://twitter.com/"><img src="/src/icons8-twitter-80.png" alt="" width={40}/></a>
+            <a href="https://github.com/"><img src="/icons8-github-80.png" alt="" width={40}/></a>
+            <a href="https://www.instagram.com/"><img src="/icons8-instagram-80 (1).png" alt="" width={40}/></a>
+            <a href="https://www.facebook.com/"><img src="/icons8-facebook-80.png" alt="" width={40}/></a>
+            <a href="https://twitter.com/"><img src="/icons8-twitter-80.png" alt="" width={40}/></a>
         </div>
     )
 }
